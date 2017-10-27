c02d0ccb2cf45c97e4f155f7123cf24044f307a1
Rob Hess <robwhess@gmail.com>
2017-10-27 16:25:07 -0700

    Add code from 10/27 lecture.

--- wordBoxes.js
+++ wordBoxes.js
@@ -12,10 +12,34 @@ function generateWordElem(word, highlightColor) {
   var wordSpan = document.createElement('span');
   wordSpan.classList.add('word');
   wordSpan.textContent = word;
+
+  if (highlightColor) {
+    wordSpan.classList.add('highlight');
+    wordSpan.classList.add(highlightColor);
+  }
+
   return wordSpan;
 }
 
 function getHighlightColor() {
+
+  // var highlightColor = null;
+  // var highlightColorInputs = [
+  //   document.getElementById('highlight-color-red'),
+  //   document.getElementById('highlight-color-green'),
+  //   document.getElementById('highlight-color-blue')
+  // ];
+  //
+  // highlightColorInputs.forEach(function (input) {
+  //   if (input.checked) {
+  //     highlightColor = input.value;
+  //   }
+  // });
+  // return highlightColor;
+
+  var selectedColorInput = document.querySelector('input[name="highlight-color"]:checked');
+
+  return selectedColorInput.value;
 
 }
 
@@ -26,7 +50,14 @@ function handleAddWordButtonClick(event) {
   if (word) {
     console.log("== handling add word button click, word:", word);
 
+    var everyNthSelect = document.getElementById('every-nth-select');
+    var everyNth = parseInt(everyNthSelect.value);
+    // console.log("== every nth:", everyNth);
+
     var highlightColor = null;
+    if (!((currentWord + 1) % everyNth)) {
+      highlightColor = getHighlightColor();
+    }
 
     var wordElem = generateWordElem(word, highlightColor);
     var container = event.currentTarget.parentNode.parentNode;
@@ -42,6 +73,12 @@ function handleWordsContainerClick(event) {
 
   var wordsContainer = event.currentTarget;
   var clickedElem = event.target;
+
+  // console.log("== wordsContainer:", wordsContainer);
+  // console.log("== clickedElem:", clickedElem);
+  if (clickedElem.classList.contains('word')) {
+    wordsContainer.removeChild(clickedElem);
+  }
 
 }
 
